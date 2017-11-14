70ca95d85488d07400aac2dbfecdb58d42ec8c7c
apptest <appTest1@wavemaker.com>
2017-11-14 20:46:57 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_jxgFIaXMPM",
+  "homePage" : "Main",
+  "name" : "Auto_jxgFIaXMPM",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
